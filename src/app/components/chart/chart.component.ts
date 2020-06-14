4d4d3ae69bbf63136b68417b030cccedf466becb
Jaidiber <jaider.9507@gmail.com>
2020-06-13 19:05:56 -0500

    refactor chart component

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Chart } from 'chart.js';
 
 @Component({
@@ -8,11 +8,16 @@ import { Chart } from 'chart.js';
 })
 export class ChartComponent implements OnInit {
 
-  device = "dispositivo"
+  @Input()
+  nameDevice: String;
 
-  data = [250, 219, 175, 137, 79, 50, 194, 108, 238, 72, 64, 108, 160, 184, 151, 32, 216, 84, 195, 40, 171, 195, 141, 7, 91, 200, 33, 208, 148, 171, 236, 23, 118, 242, 178, 213, 143, 179, 193, 85, 23, 95, 208, 34, 215, 149, 244, 215, 175, 86, 110, 64, 225, 107, 62, 166, 80, 1, 225, 123, 8, 5, 170, 71, 34, 34, 5, 51, 236, 23, 5, 152, 32, 102, 237, 126, 119, 189, 131, 83, 43, 16, 219, 100, 13, 24, 56, 237, 120, 102, 246, 121, 226, 178, 218, 166, 59, 198, 33, 27];
+  @Input()
+  values: any;
 
-  private intervalUpdate: any = null;
+  @Input()
+  dates: any;
+
+
   public chart: any = null;
 
 
@@ -22,9 +27,6 @@ export class ChartComponent implements OnInit {
     this.showData()
   }
 
-  private ngOnDestroy(): void {
-    clearInterval(this.intervalUpdate);
-  }
 
   showData() {
     this.chart = new Chart('realtime', {
@@ -67,29 +69,27 @@ export class ChartComponent implements OnInit {
         }
       }
     });
-    this.getData()//datos a mostrar
-    this.intervalUpdate = setInterval(function () {//Tiempo que dura cada salto de tiempo
-      this.getData();
-    }.bind(this), 500);
+    this.setInitialData();
   }
 
-  private getData() {
+  setInitialData(){
+    this.chart.data.labels = this.values;
+    this.chart.data.datasets[0].data= this.values;
+    this.chart.update();
+  }
 
-    if (this.data.length != 0) {//If para comprobar que hay datos necesarios
-      let chartTime: any = new Date();
-      chartTime = chartTime.getHours() + ':' + ((chartTime.getMinutes() < 10) ? '0' + chartTime.getMinutes() : chartTime.getMinutes()) + ':' + ((chartTime.getSeconds() < 10) ? '0' + chartTime.getSeconds() : chartTime.getSeconds());
-      if (this.chart.data.labels.length > 25) {//cantidad de datos en el grafico
-        this.chart.data.labels.shift();
-        this.chart.data.datasets[0].data.shift();
-      }
-      this.chart.data.labels.push(chartTime); //Eje x, eje y lo toma por defecto
-      this.chart.data.datasets[0].data.push(this.data.pop());//lugar donde se ingresa el dato o datos a graficar
-      this.chart.update();
-    } else {
-      console.error("ERROR: The response had an error, retrying");
+  onMessage(message){
+    let chartTime: any = new Date();
+    chartTime = chartTime.getHours() + ':' + ((chartTime.getMinutes() < 10) ? '0' + chartTime.getMinutes() : chartTime.getMinutes()) + ':' + ((chartTime.getSeconds() < 10) ? '0' + chartTime.getSeconds() : chartTime.getSeconds());
+    const valorDecMessage = 25;
+    if (this.chart.data.labels.length > 15) {
+      this.chart.data.labels.shift();
+      this.chart.data.datasets[0].data.shift();
     }
+    this.chart.data.labels.push(chartTime); //Eje X, el 
+    this.chart.data.datasets[0].data.push(valorDecMessage);//cada vez que llegue un mensaje de la suscripcion
+    this.chart.update();
+    
   }
-
-
 
 }
